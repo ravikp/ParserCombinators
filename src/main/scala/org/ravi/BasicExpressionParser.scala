327b423fcce67f87ea6127cd9db534bd287d6cee
Ravi Kumar Pasumarthy <ravi.pasumarthy@gmail.com>
2016-01-26 01:00:21 +0800

    Impl multiplication and division to occur multiple times

--- src/main/scala/org/ravi/BasicExpressionParser.scala
+++ src/main/scala/org/ravi/BasicExpressionParser.scala
@@ -7,8 +7,8 @@ import scala.util.parsing.combinator.JavaTokenParsers
  */
 trait BasicExpressionParser extends JavaTokenParsers{
 
-  def expr:Parser[Any] = mult ~ rep("+" ~ mult | "-" ~ mult)
-  def mult = factor ~ "*" ~ factor | factor
+  def expr:Parser[Any] = muldiv ~ rep("+" ~ muldiv | "-" ~ muldiv)
+  def muldiv = factor ~ rep("[*/]".r ~ factor) | factor
 
   def factor = wholeNumber | "(" ~ expr ~ ")"
 
