8dc224e7f8c5d1f0bdb0dce5c3199d50cce35493
Ravi Kumar Pasumarthy <ravikup@thoughtworks.com>
2016-01-26 00:29:51 +0800

    Add testcase to subtract two variables

--- src/test/scala/org/ravi/BasicExpressionParserSpec.scala
+++ src/test/scala/org/ravi/BasicExpressionParserSpec.scala
@@ -10,4 +10,8 @@ class BasicExpressionParserSpec extends FlatSpec with Matchers with BasicExpress
   "Parse" should "parse simple additions of two variables" in {
     parseAll(expr, "3 + 5").successful shouldBe true
   }
+
+  it should "subtract two variables" in {
+    parseAll(expr, "3 - 5").successful shouldBe true
+  }
 }
