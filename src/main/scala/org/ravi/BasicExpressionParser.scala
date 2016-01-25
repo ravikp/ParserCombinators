d851f4d8666212ecaf27b92647c3f15e2951650d
Ravi Kumar Pasumarthy <ravikup@thoughtworks.com>
2016-01-26 00:30:51 +0800

    Implement addition and subtraction of two variables

--- src/main/scala/org/ravi/BasicExpressionParser.scala
+++ src/main/scala/org/ravi/BasicExpressionParser.scala
@@ -7,6 +7,6 @@ import scala.util.parsing.combinator.JavaTokenParsers
  */
 trait BasicExpressionParser extends JavaTokenParsers{
 
-  def expr:Parser[Any] = wholeNumber ~ "+" ~ wholeNumber
+  def expr:Parser[Any] = wholeNumber ~ ("+" ~ wholeNumber | "-" ~ wholeNumber)
 
 }
