4a2f19b24a0fcb352290780bb38e43a8facbc840
Ravi Kumar Pasumarthy <ravikup@thoughtworks.com>
2016-01-26 00:27:24 +0800

    Test case to parse addition of two numbers

---
+++ src/main/scala/org/ravi/BasicExpressionParser.scala
@@ -0,0 +1,12 @@
+package org.ravi
+
+import scala.util.parsing.combinator.JavaTokenParsers
+
+/**
+ * Created by ravikupin on 26/1/16.
+ */
+trait BasicExpressionParser extends JavaTokenParsers{
+
+  def expr:Parser[Any] = ???
+
+}
---
+++ src/test/scala/org/ravi/BasicExpressionParserSpec.scala
@@ -0,0 +1,13 @@
+package org.ravi
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+ * Created by ravikupin on 26/1/16.
+ */
+
+class BasicExpressionParserSpec extends FlatSpec with Matchers with BasicExpressionParser {
+  "Parse" should "parse simple additions of two variables" in {
+    parseAll(expr, "3 + 5").successful shouldBe true
+  }
+}
